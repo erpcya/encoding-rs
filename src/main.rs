e8871059e710ec0a2c551f9bc87a2b49a410f09c
Yamel Senih <ysenih@erpya.com>
2022-11-11 23:46:03 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@
 fn main() {
  let sparkle_heart = vec![166, 202, 57, 4, 130, 48, 130, 49, 147, 226, 48, 76, 147, 26, 48, 40, 147, 254, 2, 10, 226, 48, 130, 48, 4, 130, 48, 226, 52, 81, 4, 130];
  println!("Epa 1: {:?}", sparkle_heart);
- let sparkle_heart = String::from_utf8_lossy(&sparkle_heart);
+ let sparkle_heart = String::from_utf16(&sparkle_heart);
  println!("Epa 2: {:?}", sparkle_heart);
 }
